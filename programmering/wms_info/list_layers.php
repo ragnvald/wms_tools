ba5694eb400d9563f453e5aa04fc873e283a3522
Ragnvald Larsen <ragnvald@mindland.com>
2013-10-30 00:59:17 +0100

    First fully functional version

--- programmering/wms_info/list_layers.php
+++ programmering/wms_info/list_layers.php
@@ -175,18 +175,23 @@ $map_center_y_native =(($miny_native + $maxy_native) / 2);
 
 $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_native;
 
-//Here the HTML code starts
-?><!DOCTYPE html>
+//HTML ode starts ?>
+<!DOCTYPE html>
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Spatial data for the Geoserver workspace: <?php echo $select_workspace; ?></title>
 
-        <script src = "http://www.openlayers.org/api/OpenLayers.js"></script>
-        <script src='http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAl9RMqSzhPUXAfeBCXOussRSPP9rEdPLw3W8siaiuHC3ED5y09RTJKbutSNVCYFKU-GnzKsHwbJ3SUw'></script>
-<!-- Load the OpenLayers API library and stylesheet -->
-        <link rel="stylesheet" href="OpenLayers-2.7/theme/default/style.css" type="text/css" />
-
+        <script src ='http://openlayers.org/api/OpenLayers.js'></script>
+        
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="stylesheet" href="include/default_style.css" type="text/css">
+        
+        <script src ='http://maps.google.com/maps?file=api&v=2&key=ABQIAAAAl9RMqSzhPUXAfeBCXOussRSPP9rEdPLw3W8siaiuHC3ED5y09RTJKbutSNVCYFKU-GnzKsHwbJ3SUw'></script>
+       
         <script type = "text/javascript">
+            var map, layer;
+            
             var lon = <?php echo $map_center_x_geo ?>;
             var lat = <?php echo $map_center_y_geo ?>;
 
@@ -197,71 +202,69 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
             var map;
 
             function init(){
-
-                map_controls = [ new OpenLayers.Control.OverviewMap(),       
-                    new OpenLayers.Control.LayerSwitcher({'ascending':true}),
-                    new OpenLayers.Control.PanZoomBar(),
-                    new OpenLayers.Control.KeyboardDefaults()];
-
-                map = new OpenLayers.Map( 'map', {controls: map_controls} );
-
-                var ls = map.getControlsByClass('OpenLayers.Control.LayerSwitcher')[0];
-
-                ls.maximizeControl();
-
-
-                    var gmap = new OpenLayers.Layer.Google(
-                        "Google Streets" // the default
-                    );
-                    var gsat = new OpenLayers.Layer.Google(
-                        "Google Satellite",
-                        {type: G_SATELLITE_MAP}
-                    );
-                    map.addLayers([gmap, gsat]);<?php 
+                
+               var geographic = new OpenLayers.Projection("EPSG:4326");
+               var mercator = new OpenLayers.Projection("EPSG:900913");
+                                
+               map = new OpenLayers.Map( 'map',{projection: mercator});
+                
+               map.addControl(new OpenLayers.Control.LayerSwitcher());
+            
+               layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+                    map.addLayer(layer);
+                    map.setCenter(
+                        new OpenLayers.LonLat(9.147, 65.472).transform(
+                            new OpenLayers.Projection("EPSG:4326"),
+                            map.getProjectionObject()
+                        ), 4
+                    );    
+                
+                <?php 
 
                 // Add layers according to available layers in Geoserver
                 // Uses lonlat 
                 $i=0;
                 
                 foreach ($caps->layers as $l) {
+                    //if (1==2) {      
                     if ($l['queryable']) {          
+                    
                         //Filter out layers which are similar to the one 
-                        if  (substr(($l['Name']),0,$select_workspace_length)==$select_workspace) {
+                        if  (substr(($l['Name']),0,$select_workspace_length)==$select_workspace) {          
                             
+                            $srs_geo = (array_keys($d['BoundingBox']));?>
                             
-                            $srs_latlon = (array_keys($d['BoundingBox']));
-                            //echo $srs_latlon[0];?>    
-                            
-                            wms_layer_<?php echo $i; ?> = new OpenLayers.Layer.WMS("<?php echo $l['Name'] ?>",
-                                "http://wms.dirnat.no/geoserver/ows?service=wms",
+                            var wms_layer_<?php echo $i; ?> = new OpenLayers.Layer.WMS("<?php echo $l['Name'] ?>",
+                                "http://wms.dirnat.no/geoserver/ows?service=wms&version=1.1.1",
                                 {
-                                    "layers":"<?php echo $l['Name'] ?>",
-                                    "format":"image/png", 
-                                    "transparent": "true"
-                                }
-                                //,
-                                //{ "reproject":"true" }
+                                    layers:'<?php echo $l['Name'] ?>',
+                                    styles:'',
+                                    srs:'EPSG:4326',
+                                    format:'image/png', 
+                                    transparent: "true"
+                                },
+                                {opacity: 1.0},
+                                {isBaseLayer: false, visibility: true}
                             );
 
+                            wms_layer_<?php echo $i; ?>.setVisibility(true); 
+                             
                             map.addLayer(wms_layer_<?php echo $i; ?>);
 
-                            wms_layer_<?php echo $i; ?>.setVisibility(false);  
 
                             <?php $i++;
                         }  
                     } 
 
                 }?>                                                                                                                 
-                map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);     
+                //map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);     
             }
         </script>
-    </head>
-
-    <body onload = "init()">
+     </head>
+     <body onload = "init()">
         <h1 id = "title">Data set presentation</h1>
-        <div id="map" class="bigmap"></div>        
+        <div id="map" class="smallmap"></div>
         <h3><strong>Layers count:</strong><?php echo ($i - 1) ?></h3>
-
         <ol>
             <?php
             $i=1;
@@ -275,6 +278,7 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                     
                     {
                         
+                        //Handle native srs based information
                         $srs_native = (array_keys($l['BoundingBox']));
                                             
                         if (isset($l['BoundingBox'][$srs_native[0]]['minx'])) 
@@ -302,8 +306,39 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                             $current_maxy_t = $l['BoundingBox'][$srs_native[0]]['maxy']; 
                             $current_maxy = floatval($current_maxy_t);
                         }    
-
+                        
                         $boundingbox_native = $current_minx.",".$current_miny.",".$current_maxx.",".$current_maxy;
+                        
+                        //Handle geographinc information
+                                            
+                        if (isset($l['LatLonBoundingBox']['minx'])) 
+                        {
+                            $current_minx_t = $l['LatLonBoundingBox']['minx'];  
+                            
+                            $current_minx = floatval($current_minx_t);
+                        }
+
+                        if (isset($l['LatLonBoundingBox']['miny'])) 
+                        {
+                            $current_miny_t = $l['LatLonBoundingBox']['miny']; 
+                            
+                            $current_miny = floatval($current_miny_t);
+                        }
+
+                        if (isset($l['LatLonBoundingBox'][$srs_native[0]]['maxx'])) 
+                        {
+                            $current_maxx_t = $l['LatLonBoundingBox']['maxx'];
+                            $current_maxx = floatval($current_maxx_t);  
+                        }
+
+                        if (isset($l['LatLonBoundingBox']['maxy'])) 
+                        {
+                            $current_maxy_t = $l['LatLonBoundingBox']['maxy']; 
+                            $current_maxy = floatval($current_maxy_t);
+                        }    
+
+
+                        $boundingbox_geo = $current_minx.",".$current_miny.",".$current_maxx.",".$current_maxy;
                         
                         ?>
 
@@ -320,16 +355,13 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                                         <td>
                                             <?php
                                             echo $l['Abstract']?>
-
                                             <br>
-                                            Bounding box: (<i><?php echo $boundingbox_native; ?>)</i>    
-
+                                            <b>Bounding box:</b><br>
+                                            <i><?php echo $srs_native[0].": ".$boundingbox_native; ?>)</i><br> 
+                                            <i><?php echo "Geograpihcal: ".$boundingbox_geo; ?>)</i>
                                         </td>
-
                                     </tr>
-
                                 </table>
-
                             <br/></li>
 
                         <?php
@@ -338,6 +370,6 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                 }
             }
             ?>
-        </ol>
+    </ol>
     </body>
 </html>
