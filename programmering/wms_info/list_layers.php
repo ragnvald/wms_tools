9f53c860e021e7ad7d2e13b649a340d4e423db2e
Ragnvald Larsen <ragnvald@mindland.com>
2013-10-30 13:39:57 +0100

    Dynamic centerpoint in javascript added

--- programmering/wms_info/list_layers.php
+++ programmering/wms_info/list_layers.php
@@ -213,7 +213,7 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
                     map.addLayer(layer);
                     map.setCenter(
-                        new OpenLayers.LonLat(9.147, 65.472).transform(
+                        new OpenLayers.LonLat(lon,lat).transform(
                             new OpenLayers.Projection("EPSG:4326"),
                             map.getProjectionObject()
                         ), 4
