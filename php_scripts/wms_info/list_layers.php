9a42980b494ec5b4bc0e8795c89d3f87d8dad219
Ragnvald Larsen <ragnvald@mindland.com>
2013-11-05 11:08:02 +0100

    Generalising

--- php_scripts/wms_info/list_layers.php
+++ php_scripts/wms_info/list_layers.php
@@ -11,17 +11,18 @@
 ///////////////////////////////////////////////////////////////////
 
 
-
-
 //Get workspace request 
 
-//$select_workspace=(isset($_GET['ws']) ? $_GET['ws'] : null);
-
+$select_workspace=(isset($_GET['ws']) ? $_GET['ws'] : null);
 
 //Do the default workspace if the request returns empty
 if (empty($select_workspace))
 {
-    $select_workspace="inon";
+    $select_workspace="geonode";
+}
+// The workspace has been set. Ready to roll...
+else {
+    
 }
 
 $domain = $select_workspace;
@@ -31,7 +32,7 @@ $select_workspace = $select_workspace.":";
 
 //Set variables
 $thumbnail_maxx             = 200;
-$wms_server                 = "http://wms.dirnat.no/geoserver/";
+$wms_server                 = "http://54.235.206.158:80/geoserver/";
 $wms_server_ows             = $wms_server."ows?";
 $wms_server_getcapabilities = $wms_server_ows."service=wms&version=1.1.1&request=GetCapabilities";
 
@@ -237,7 +238,7 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                             
                             //Addning layer for <?php echo $l['Name'] ?> separately
                             var wms_layer_<?php echo $i; ?> = new OpenLayers.Layer.WMS("<?php echo $l['Name'] ?>",
-                                "http://wms.dirnat.no/geoserver/ows?service=wms&version=1.1.1",
+                                "<?php echo $wms_server_ows ?>service=wms&version=1.1.1",
                                 {
                                     layers:'<?php echo $l['Name'] ?>',
                                     styles:'',
@@ -346,15 +347,14 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                                 <table>
                                     <tr>
                                         <td valign=top>
-                                        <?php 
-                                        if (strlen($l['Abstract'])>0) {
-                                            echo "Abstract<br>";
-                                            echo $l['Abstract']."<br>";
-                                        }
-                                        ?>
                                         <img src="<?php echo $wms_server ?><?php echo $domain ?>/wms?service=WMS&version=1.1.0&request=GetMap&layers=<?php echo $l['Name'] ?>&styles=&bbox=<?php echo $boundingbox_native;?>&width=<?php echo $thumbnail_maxx ?>&height=<?php echo $thumbnail_maxy ?>&srs=<?php echo $srs_native[0]?>&format=image/png"><hr>
                                         </td>
-                                        <td><a href=<?php echo $wms_server_ows ?>service=wms&version=1.1.1&request=GetMap&layers=<?php echo $l['Name'] ?>&styles=&bbox=<?php echo $boundingbox_native;?>&width=512&height=469&srs=<?php echo $srs_native[0] ?>&format=application/openlayers><img src="graphics/icon_link.png" border="0" width="16" height="16" alt="icon_link.png (343 bytes)">View on server</a>
+                                        <td valign=top><?php 
+                                        if (strlen($l['Abstract'])>0) {
+                                            echo "Abstract<br>";
+                                            echo $l['Abstract']."<br><br>";
+                                        }
+                                        ?><a href=<?php echo $wms_server_ows ?>service=wms&version=1.1.1&request=GetMap&layers=<?php echo $l['Name'] ?>&styles=&bbox=<?php echo $boundingbox_native;?>&width=512&height=469&srs=<?php echo $srs_native[0] ?>&format=application/openlayers><img src="graphics/icon_link.png" border="0" width="16" height="16" alt="icon_link.png (343 bytes)">View on server</a>
                                         <br>
                                         <br>  
                             <a href="<?php echo $wms_server_ows ?>service=WFS&version=1.0.0&request=GetFeature&typeName=<?php echo $l['Name'] ?>&outputFormat=SHAPE-ZIP"><img src="graphics/icon_download.png" border="0" width="16" height="16" alt="icon_download.png (1?159 bytes)">Download shapefile</a><br>                                           
@@ -362,7 +362,7 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td><img src = "<?php echo $wms_server_ows ?>service=wms&REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=<?php echo $l['Name'] ?>">
+                                        <td><b>Legend:</b><br><img src = "<?php echo $wms_server_ows ?>service=wms&REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=<?php echo $l['Name'] ?>">
                                         </td>
                                         <td valign=bottom>
                                         <b>Bounding box:</b><br>
@@ -380,5 +380,7 @@ $boundingbox_native = $minx_native.",".$miny_native.",".$maxx_native.",".$maxy_n
                 }
             }?>
     </ol>
+    <br>
+    <i>Version 0.6</i>
     </body>
 </html>
